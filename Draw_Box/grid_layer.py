91bb35c18ad860534e9e61ef32e1c7dcb23a99d7
DenisUreke <70695931+DenisUreke@users.noreply.github.com>
2025-07-18 22:51:09 +0200

    before i fuck it up

---
+++ Draw_Box/grid_layer.py
@@ -0,0 +1,29 @@
+from PySide6.QtWidgets import QWidget
+from PySide6.QtGui import QPainter, QPixmap, QMouseEvent
+from PySide6.QtCore import Qt, QRect
+from Models.main_data_model import MainDataModel
+
+class GridLayer(QWidget):
+    def __init__(self, tile_selector, model: MainDataModel, parent=None):
+        super().__init__(parent)
+        self.tile_selector = tile_selector
+        self.model = model
+        self.grid = model.grid
+        
+        
+        self.setAttribute(Qt.WA_TransparentForMouseEvents, True)
+        self.setFixedHeight(900)
+        self.setFixedWidth(965)
+        
+    def paintEvent(self, event):
+        painter = QPainter(self)
+
+        for y in range(self.model.setup_data_model.grid_height):
+            for x in range(self.model.setup_data_model.grid_width):
+                painter.setPen(Qt.gray)
+                painter.drawRect(
+                    x * self.model.setup_data_model.tile_size_x,
+                    y * self.model.setup_data_model.tile_size_y,
+                    self.model.setup_data_model.tile_size_x,
+                    self.model.setup_data_model.tile_size_y
+                )
